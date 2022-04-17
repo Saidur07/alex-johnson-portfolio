41cdf2cf07f2eee06a4429e97abc7dbf138fb5c7
Saidur-07 <md.saidurrahman.bhola2007@gmail.com>
2022-04-17 11:04:21 +0600

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
+
 function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <Navbar></Navbar>
+      <Header></Header>
+      <Banner></Banner>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("flowbite/plugin")],
   daisyui: {
     themes: false,
   },
